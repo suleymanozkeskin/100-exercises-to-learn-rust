56a083cbac4526fe64b601fd28fb80540ea2b7ee
Worklane-GmbH <suleyman@worklane.de>
2024-10-01 14:56:43 +0200

    - initial commit

--- exercises/02_basic_calculator/01_integers/src/lib.rs
+++ exercises/02_basic_calculator/01_integers/src/lib.rs
@@ -1,6 +1,7 @@
 fn compute(a: u32, b: u32) -> u32 {
     // TODO: change the line below to fix the compiler error and make the tests pass.
-    a + b * 4u8
+    let my_value: u32 = 4;
+    a + b * my_value
 }
 
 #[cfg(test)]
